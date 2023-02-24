6e396b842d12881cb369d51853263a9386ea1d14
Augusto Cesar <acs-03@hotmail.com>
2023-02-24 17:50:05 -0300

    Adicionado informações no token

--- server/src/main/java/br/comvarejonline/projetoinicial/config/AuthorizationServerConfig.java
+++ server/src/main/java/br/comvarejonline/projetoinicial/config/AuthorizationServerConfig.java
@@ -1,5 +1,8 @@
 package br.comvarejonline.projetoinicial.config;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -8,12 +11,26 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+import br.comvarejonline.projetoinicial.components.JwtTokenEnhancer;
 
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    @Value("${security.oauth2.client.client-id}")
+    private String clientId;
+
+    @Value("${security.oauth2.client.client-secret}")
+    private String clientSecret;
+
+    @Value("${jwt.duration}")
+    private Integer jwtDuration;
+
+    private JwtTokenEnhancer jwtTokenEnhancer;
 
     private BCryptPasswordEncoder passwordEncoder;
     private JwtAccessTokenConverter accessTokenConverter;
@@ -22,11 +39,12 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
     public AuthorizationServerConfig(BCryptPasswordEncoder passwordEncoder,
             JwtAccessTokenConverter accessTokenConverter, JwtTokenStore tokenStore,
-            AuthenticationManager authenticationManager) {
+            AuthenticationManager authenticationManager, JwtTokenEnhancer jwtTokenEnhancer) {
         this.passwordEncoder = passwordEncoder;
         this.accessTokenConverter = accessTokenConverter;
         this.tokenStore = tokenStore;
         this.authenticationManager = authenticationManager;
+        this.jwtTokenEnhancer = jwtTokenEnhancer;
     }
 
     @Override
@@ -37,18 +55,22 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients.inMemory()
-                .withClient("varejonline")
-                .secret(passwordEncoder.encode("varejonline123"))
+                .withClient(clientId)
+                .secret(passwordEncoder.encode(clientSecret))
                 .scopes("read", "write")
                 .authorizedGrantTypes("password")
-                .accessTokenValiditySeconds(86400);
+                .accessTokenValiditySeconds(jwtDuration);
     }
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        TokenEnhancerChain chain = new TokenEnhancerChain();
+        chain.setTokenEnhancers(Arrays.asList(accessTokenConverter, jwtTokenEnhancer));
+
         endpoints.authenticationManager(authenticationManager)
                 .tokenStore(tokenStore)
-                .accessTokenConverter(accessTokenConverter);
+                .accessTokenConverter(accessTokenConverter)
+                .tokenEnhancer(chain);
     }
 
 }
