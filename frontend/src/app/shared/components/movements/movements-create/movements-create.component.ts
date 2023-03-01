477a83dc6996706e3bd503538f485234febd66b5
Augusto Cesar <acs-03@hotmail.com>
2023-03-01 17:46:13 -0300

    Frontend - Iniciando criação do objeto movement para salvar no banco

--- frontend/src/app/shared/components/movements/movements-create/movements-create.component.ts
+++ frontend/src/app/shared/components/movements/movements-create/movements-create.component.ts
@@ -10,6 +10,9 @@ import { IToken } from 'src/app/shared/models/token.model';
 import { IProduct } from 'src/app/shared/models/product.model';
 import { ITypeMovement } from 'src/app/shared/models/type-movement.model';
 import { IMovement } from 'src/app/shared/models/movement.model';
+import { IMovementCreate } from 'src/app/shared/models/movement-create.model';
+import { IRole } from 'src/app/shared/models/role.model';
+import { IUser } from 'src/app/shared/models/user.model';
 
 @Component({
   selector: 'app-movements-create',
@@ -23,16 +26,21 @@ export class MovementsCreateComponent implements OnInit {
   public listMovements:Array<IMovement> = [];
   public dateMovement:string;
   public typeMovementId:number;
-  public quantityMovement:number;
-  public reasonMovement:string;
-  public documentMovement:number;
+  public quantityMovement:number = 1;
+  public reasonMovement:string = 'teste';
+  public documentMovement:number = 1;
   public hasProduct:boolean = false;
+  public isQuantityValid:boolean = true;
   public userRole:string = "";
   public tokenDecoded:IToken;
+  public role:IRole;
+  public typeMovement:ITypeMovement;
+  public user:IUser;
   public product:IProduct;
   public productId:number;
   public productName:string;
   public productBalance:number;
+  public movementCreate:IMovementCreate = null;
 
   constructor(
     private formBuilder:FormBuilder,
@@ -55,15 +63,15 @@ export class MovementsCreateComponent implements OnInit {
 
   private createForm() {
     return this.formBuilder.group({
-      productId:["", Validators.required],
-      productName:[null, ],
-      productBalance:[null, ],
-      dateMovement:[null, ],
-      typeMovement:[null, ],
-      typeMovementId:[null, ],
-      quantityMovement:[null, ],
-      reasonMovement:[null, ],
-      documentMovement:[null, ],
+      productId:[null,Validators.required],
+      productName:[null,],
+      productBalance:[null,],
+      dateMovement:[null,Validators.required],
+      typeMovement:[null,Validators.required],
+      typeMovementId:[null,],
+      quantityMovement:[null,Validators.required],
+      reasonMovement:[null,Validators.required],
+      documentMovement:[null,],
     })
   }
 
@@ -73,7 +81,7 @@ export class MovementsCreateComponent implements OnInit {
       this.form.get('dateMovement').enable();
       this.form.get('quantityMovement').enable();
       this.form.get('reasonMovement').enable();
-      this.form.get('documentMovement').enable();
+      this.form.get('documentMovement').disable();
     }else{
       this.form.get('typeMovement').disable();
       this.form.get('dateMovement').disable();
@@ -95,6 +103,8 @@ export class MovementsCreateComponent implements OnInit {
             this.listMovements = res;
             if(this.listMovements.length === 0 && this.userRole === 'ROLE_OPERATOR'){
               this.toast.warning("Esse produto não possui saldo inicial, contante um gerente!");
+              this.productName = "";
+              this.productBalance = null;
               this.listTypeMovements = [];
               this.blockImputis(true);
               return;
@@ -127,15 +137,43 @@ export class MovementsCreateComponent implements OnInit {
     return !!(this.form.get(controlName)?.invalid && this.form.get(controlName)?.touched);
   }
 
+  public checkQuantity(){
+    if(this.product.balance < this.quantityMovement){
+      this.form.controls['quantityMovement'].setErrors({'incorrect': true});
+      this.isQuantityValid = false;
+    }else{
+      this.form.controls['quantityMovement'].setErrors(null);
+      this.isQuantityValid = true;
+    }
+  }
+
+  public checkTypeMovement(){
+    if(this.typeMovementId === 2 || this.typeMovementId === 3){
+      this.form.get('documentMovement').enable();
+    }else{
+      this.form.get('documentMovement').disable();
+    }
+  }
+
+  public createMovement(product:IProduct){
+    this.getTypeMovement();
+    this.movementCreate.typeMovement = this.typeMovement;
+  }
+
+  public getTypeMovement(){
+    this.listTypeMovements.map(
+      type => {if(type.id === this.typeMovementId){
+        this.typeMovement = type;
+      }}
+    )
+  }
+
   public save():void{
     console.log(this.dateMovement);
     console.log(this.typeMovementId);
-    console.log(this.quantityMovement);
+    console.log(this.listTypeMovements);
     console.log(this.reasonMovement);
     console.log(this.documentMovement);
-    if(this.product.balance < this.quantityMovement){
-      this.toast.warning("Quantidade não pode ser menor que o saldo!");
-    }
   }
 
 }
---
+++ frontend/src/app/shared/models/movement-create.model.ts
@@ -0,0 +1,15 @@
+import { Data } from "@angular/router";
+import { IProduct } from "./product.model";
+import { ITypeMovement } from "./type-movement.model";
+import { IUser } from "./user.model";
+
+export interface IMovementCreate {
+  product: IProduct;
+  typeMovement: ITypeMovement;
+  user: IUser;
+  date: Data;
+  reason: string;
+  document: number;
+  quantity: number;
+  situation: string
+}
